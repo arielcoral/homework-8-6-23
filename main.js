5c32ea1728f96a801ff9f5e1a51003c80dd33df5
arielcoral <123407136+arielcoral@users.noreply.github.com>
2023-06-14 13:34:57 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,36 @@
+// תרגיל ראשון
+
+// const button = document.querySelector("#button");
+// const img = document.querySelector("#img");
+
+// button.addEventListener("click", function(event){
+//     event.preventDefault();
+//     if(img.style.display === "none"){
+//         img.style.display = "inline";
+//         button.textContent = "Click to hide the image";
+//     } else {
+//         img.style.display = "none";
+//         button.textContent = "Click to return the image";
+//     }
+// });
+
+
+// רגיל שני
+// const balloon = document.querySelector("#balloon");
+// let size = 100
+
+// document.addEventListener("keydown", function(event) {
+//     if (event.key === "ArrowUp") {
+//         size += 10
+//         balloon.style.fontSize = `${size}px`;
+//     }
+//     else if(event.key === "ArrowDown"){
+//         size -= 10
+//         balloon.style.fontSize = `${size}px`;
+//     }
+//     if(size > 200){
+//         balloon.textContent = "💥"
+//     }
+//   });
+  
+  
